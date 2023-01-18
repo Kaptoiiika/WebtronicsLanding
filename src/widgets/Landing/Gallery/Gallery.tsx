8b52d6aea3cbb00b192063e9c0f8ea94942a2547
Nikita Ozhegov <nikita.ozhegov8@mail.ru>
2023-01-18 10:45:25 +0400

    add gallary to landing

--- src/widgets/Landing/Gallery/Gallery.tsx
+++ src/widgets/Landing/Gallery/Gallery.tsx
@@ -1,11 +1,52 @@
 import { classNames } from "@/shared/lib/classNames/classNames"
+import { VStack } from "@/shared/ui/Stack"
+import {
+  Typography,
+  TypographyVariants,
+} from "@/shared/ui/Typography/Typography"
 import styles from "./Gallery.module.scss"
+import developer1 from "@/shared/assets/images/developer 1.png"
+import developer2 from "@/shared/assets/images/developer 2.png"
+import developer3 from "@/shared/assets/images/developer 3.png"
+import developer4 from "@/shared/assets/images/developer 4.png"
 
-type GalleryProps = {
-  className?: string
-}
-
-export const Gallery = (props: GalleryProps) => {
-  const { className } = props
-  return <section className={classNames([className])}></section>
+export const Gallery = () => {
+  return (
+    <section>
+      <VStack align="center" gap="48">
+        <Typography variant={TypographyVariants.H2}>Gallery</Typography>
+        <div className={styles.galleryGrid}>
+          <Typography
+            className={styles.textTop}
+            variant={TypographyVariants.P1}
+          >
+            By the end of this course, you will be able to develop and publish
+            your very own Google Chrome extension! In this course we will focus
+            on coding exercises and projects.
+          </Typography>
+          <img
+            className={classNames([styles.images, styles.bigimage])}
+            src={developer1.src}
+          />
+          <img className={styles.images} src={developer2.src} />
+          <img className={styles.images} src={developer3.src} />
+          <img
+            className={classNames([styles.images, styles.mediumimage])}
+            src={developer4.src}
+          />
+          <Typography
+            className={styles.textBottom}
+            variant={TypographyVariants.P1}
+          >
+            If you would like to learn web development and get a job in the tech
+            industry, you are going to LOVE this course! Learn HTML, CSS,
+            JavaScript, Bootstrap and more with over 15 hours of HD video
+            tutorials! This course was designed to be extremely beginner
+            friendly. We will begin with the very basics of HTML and build a
+            simple web page.
+          </Typography>
+        </div>
+      </VStack>
+    </section>
+  )
 }
